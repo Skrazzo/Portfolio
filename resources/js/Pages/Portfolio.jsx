5bdb054d22870b29ca11e3f5d523d51e68f85861
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-05-26 23:52:08 +0300

    Added first icon
    im too tired to continue anth

--- resources/js/Pages/Portfolio.jsx
+++ resources/js/Pages/Portfolio.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import IconEarth from './Icons/IconEarth'
 
 export default function Portfolio() {
-    
-
     return (
-        <p className='text-center bg-green'>Hello world</p>
+        <>
+            <IconEarth />
+        </>
     )
 }
