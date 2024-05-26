6ec9cdec58e0c856ac15299bf9abb509f7c3ca3d
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-05-26 23:06:27 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(), // React plugin that we installed for vite.js
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            refresh: true,
+        }),
+    ],
+});
