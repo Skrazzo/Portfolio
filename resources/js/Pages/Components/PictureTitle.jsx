998718c104767b6f1d8f0a9c218f56d37df5b63c
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-05-28 18:27:28 +0300

    s

--- resources/js/Pages/Components/PictureTitle.jsx
+++ resources/js/Pages/Components/PictureTitle.jsx
@@ -4,17 +4,17 @@ import React from "react";
 export default function PictureTitle() {
     return (
         <main className="flex justify-between px-12">
-            <figure className="aspect-square relative">
-                <div className="absolute w-full h-full bg-bg-green/25"></div>
-                <img src={"me.png"} alt="me"/>
+            <figure className="relative aspect-square">
+                <div className="absolute h-full w-full bg-bg-green/25"></div>
+                <img src={"me.png"} alt="me" />
             </figure>
 
-            <div className="text-right pt-28">
+            <div className="pt-28 text-right">
                 <p className="text-3xl text-text-green">Leons Aleksandrovs</p>
                 <h1 className="text-6.25xl font-extrabold ">
                     Full-stack developer
                 </h1>
-                <button className="text-bg-green font-bold text-1.75xl rounded-rem bg-red-400 px-8 py-4 mt-8">
+                <button className="mt-8 rounded-rem bg-red-400 px-8 py-4 text-1.75xl font-bold text-bg-green">
                     CONTACT ME
                 </button>
             </div>
