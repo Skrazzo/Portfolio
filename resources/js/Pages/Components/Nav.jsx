34c4759a58924343129f20fa7f5a7dd1cb40c9d0
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-05-28 18:17:32 +0300

    Used some prettier

--- resources/js/Pages/Components/Nav.jsx
+++ resources/js/Pages/Components/Nav.jsx
@@ -1,31 +1,53 @@
-import React from 'react'
-import IconEarth from '../Icons/IconEarth'
-import TypeComponent from './TypeComponent'
-import IconSchool from '../Icons/IconSchool';
-import IconDeployedCode from '../Icons/IconDeployedCode';
-import IconContactMe from '../Icons/IconContactMe';
+import React from "react";
+import IconEarth from "../Icons/IconEarth";
+import TypeComponent from "./TypeComponent";
+import IconSchool from "../Icons/IconSchool";
+import IconDeployedCode from "../Icons/IconDeployedCode";
+import IconContactMe from "../Icons/IconContactMe";
 
 export default function Nav() {
-    
-    const NavLink = ({ text, icon }) => 
-    <div className={`text-text-green-light flex gap-2 hover:text-text-green duration-150 items-center text-2xl select-none cursor-pointer rounded-full`}>
-        {icon}
-        <span>{text}</span>
-    </div>;
+    const NavLink = ({ text, icon }) => (
+        <div
+            className={`text-text-green-light flex gap-2 hover:text-text-green duration-150 items-center text-2xl select-none cursor-pointer rounded-full`}
+        >
+            {icon}
+            <span>{text}</span>
+        </div>
+    );
 
     return (
-        <nav className='px-12 py-8 flex justify-between'>
-            <div className='flex gap-4 items-center'>
+        <nav className="px-12 py-8 flex justify-between">
+            <div className="flex gap-4 items-center">
                 <IconEarth />
-                <TypeComponent sequence={[ 'Hello world', 5000, 'Welcome to my portfolio', 3500, 'Hello world']}/>
+                <TypeComponent
+                    sequence={[
+                        "Hello world",
+                        5000,
+                        "Welcome to my portfolio",
+                        3500,
+                        "Hello world",
+                    ]}
+                />
             </div>
 
-            <div className='bg-bg-green-light/50 flex gap-8 px-8 rounded-full border-4 border-bg-green-light'>
-                <NavLink text={'Hello World'} icon={<IconEarth className={'w-8'}/>}/>
-                <NavLink text={'My Skills'} icon={<IconSchool className={'w-8'}/>} />
-                <NavLink text={'My Projects'} icon={<IconDeployedCode className={'w-8'}/>}/>
-                <NavLink text={'Contact Me'} icon={<IconContactMe className={'w-8'}/>} />
+            <div className="bg-bg-green-light/50 flex gap-8 px-8 rounded-full border-4 border-bg-green-light">
+                <NavLink
+                    text={"Hello World"}
+                    icon={<IconEarth className={"w-8"} />}
+                />
+                <NavLink
+                    text={"My Skills"}
+                    icon={<IconSchool className={"w-8"} />}
+                />
+                <NavLink
+                    text={"My Projects"}
+                    icon={<IconDeployedCode className={"w-8"} />}
+                />
+                <NavLink
+                    text={"Contact Me"}
+                    icon={<IconContactMe className={"w-8"} />}
+                />
             </div>
         </nav>
-    )
+    );
 }
--- resources/js/Pages/Components/PictureTitle.jsx
+++ resources/js/Pages/Components/PictureTitle.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 // import mePic from 'me.png';
 
 export default function PictureTitle() {
     return (
-        <main className='flex justify-between'>
-            <figure className='aspect-square relative'>
-                <div className='absolute w-full h-full bg-bg-green/25'></div>
-                <img src={'me.png'} alt="me" />
+        <main className="flex justify-between px-12">
+            <figure className="aspect-square relative">
+                <div className="absolute w-full h-full bg-bg-green/25"></div>
+                <img src={"me.png"} alt="me" />
             </figure>
 
-            <div className='text-right pt-28'>
-                <p className='text-3xl text-text-green'>Leons Aleksandrovs</p>
-                <h1 className='text-6.25xl font-extrabold'>Full-stack developer</h1>
-                <button className='text-bg-green font-bold text-1.75xl rounded-rem bg-red-400 px-8 py-4'>CONTACT ME</button>
+            <div className="text-right pt-28">
+                <p className="text-3xl text-text-green">Leons Aleksandrovs</p>
+                <h1 className="text-6.25xl font-extrabold ">
+                    Full-stack developer
+                </h1>
+                <button className="text-bg-green font-bold text-1.75xl rounded-rem bg-red-400 px-8 py-4 mt-8">
+                    CONTACT ME
+                </button>
             </div>
-
         </main>
-    )
+    );
 }
