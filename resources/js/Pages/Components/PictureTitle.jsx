5b5919254bc6c880b2c3c1a0961cc597e9db3858
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-05-28 20:24:34 +0300

    Pc side finished

--- resources/js/Pages/Components/PictureTitle.jsx
+++ resources/js/Pages/Components/PictureTitle.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-// import mePic from 'me.png';
+import Wave from "../Icons/Wave1";
 
 export default function PictureTitle() {
     return (
-        <main className="flex justify-between px-12">
-            <figure className="relative aspect-square">
-                <div className="absolute h-full w-full bg-bg-green/25"></div>
-                <img src={"me.png"} alt="me" />
-            </figure>
+        <>
+            <main className="flex justify-between px-12">
+                <figure className="relative">
+                    <div className="absolute h-full w-full bg-bg-green/25"></div>
+                    <img src={"me.png"} alt="me" />
+                </figure>
 
-            <div className="pt-28 text-right">
-                <p className="text-3xl text-text-green">Leons Aleksandrovs</p>
-                <h1 className="text-6.25xl font-extrabold ">
-                    Full-stack developer
-                </h1>
-                <button className="mt-8 rounded-rem bg-red-400 px-8 py-4 text-1.75xl font-bold text-bg-green">
-                    CONTACT ME
-                </button>
-            </div>
-        </main>
+                <div className="pt-28 text-right">
+                    <p className="text-3xl text-text-green">
+                        Leons Aleksandrovs
+                    </p>
+                    <h1 className="bg-gradient-to-r from-gradient-from to-gradient-to bg-clip-text text-6.25xl font-extrabold text-transparent">
+                        Full-stack developer
+                    </h1>
+                    <button className="mt-8 rounded-rem bg-gradient-to-r from-gradient-from to-gradient-to px-8 py-4 text-1.75xl font-bold text-bg-green">
+                        CONTACT ME
+                    </button>
+                </div>
+            </main>
+            <Wave className="absolute -translate-y-[120px] fill-bg-green-light" />
+            <Wave className="absolute -translate-y-[25px] -scale-100 fill-bg-green-light" />
+        </>
     );
 }
--- resources/js/Pages/Portfolio.jsx
+++ resources/js/Pages/Portfolio.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import IconEarth from './Icons/IconEarth'
-import Nav from './Components/Nav'
-import PictureTitle from './Components/PictureTitle'
+import React from "react";
+import IconEarth from "./Icons/IconEarth";
+import Nav from "./Components/Nav";
+import PictureTitle from "./Components/PictureTitle";
+import Wave from "./Icons/Wave1";
 
 export default function Portfolio() {
     return (
         <>
-            <div className='bg-bg-green'>
+            <div className="bg-bg-green">
                 <Nav />
                 <PictureTitle />
             </div>
         </>
-    )
+    );
 }
