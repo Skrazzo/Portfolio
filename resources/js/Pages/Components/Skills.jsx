e1636a641b604405315624cd53737f6776264e60
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-06-01 19:56:58 +0300

    Made tooltips

--- resources/js/Pages/Components/Skills.jsx
+++ resources/js/Pages/Components/Skills.jsx
@@ -3,17 +3,31 @@ import Wave from "../Icons/Wave1";
 import TypeComponent from "./TypeComponent";
 import IconSchool from "../Icons/IconSchool";
 import IconWeb from "../Icons/IconWeb";
+import Palette from "react-palette";
 
 export default function Skills() {
     const Skill = ({ toolTip = "", image = "" }) => (
-        <div className="h-20 w-20 rounded-full duration-200 hover:-translate-y-2 hover:scale-105 hover:bg-bg-green-light">
-            <img src={`./Skills/${image}`} />
-        </div>
+        <Palette src={`./Skills/${image}`}>
+            {({ data }) => (
+                <div
+                    className={`tooltip-container h-20 w-20 rounded-full  duration-200 hover:-translate-y-2 hover:scale-105 hover:bg-bg-green-light`}
+                >
+                    <div
+                        // tooltip
+                        style={{ backgroundColor: data.vibrant }}
+                        className="tooltip"
+                    >
+                        <span className="text-nowrap">{toolTip}</span>
+                    </div>
+                    <img className="object-contain" src={`./Skills/${image}`} />
+                </div>
+            )}
+        </Palette>
     );
 
     const SkillsContainer = ({ skillIcon = <></>, skills = [] }) => (
-        <div className="flex gap-6">
-            <div className="flex flex-col items-center gap-4">
+        <div className="mt-3 flex gap-6">
+            <div className="flex flex-col items-center gap-3">
                 <div className="h-20 w-20 rounded-full bg-gradient-to-b from-gradient-from to-gradient-to p-4">
                     {skillIcon}
                 </div>
@@ -47,8 +61,26 @@ export default function Skills() {
                             <IconWeb className="h-full w-full fill-bg-green" />
                         }
                         skills={new Array(20).fill({
-                            image: "scss.png",
+                            image: "react.webp",
                             name: "ReactJS",
+                        })}
+                    />
+                    <SkillsContainer
+                        skillIcon={
+                            <IconWeb className="h-full w-full fill-bg-green" />
+                        }
+                        skills={new Array(5).fill({
+                            image: "scss.png",
+                            name: "Scss",
+                        })}
+                    />
+                    <SkillsContainer
+                        skillIcon={
+                            <IconWeb className="h-full w-full fill-bg-green" />
+                        }
+                        skills={new Array(2).fill({
+                            image: "mysql.png",
+                            name: "Mysql Database",
                         })}
                     />
                 </div>
