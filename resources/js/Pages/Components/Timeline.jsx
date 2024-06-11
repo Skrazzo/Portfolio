0b887ba69df6b13aa348f1602f59e95c06e9f96b
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-06-11 16:36:55 +0300

    Finished all design for timeline

--- resources/js/Pages/Components/Timeline.jsx
+++ resources/js/Pages/Components/Timeline.jsx
@@ -8,10 +8,10 @@ export default function Timeline() {
         <>
             <Wave1 className="mt-4 translate-y-1 fill-bg-green" />
             <div className="bg-bg-green pt-1 text-white">
-                <Chapter date="May 2016" idx={1} />
-                <ChapterMobile date="May 2016" />
-                <Chapter date="September 2021" idx={2} />
-                <ChapterMobile date="September 2021" />
+                <Chapter date="May 2016" idx={1} first={true} />
+                <ChapterMobile date="May 2016" first={true} />
+                <Chapter date="September 2021" last={true} idx={2} />
+                <ChapterMobile date="September 2021" last={true} />
             </div>
         </>
     );
--- resources/js/Pages/Components/Timeline/Chapter.jsx
+++ resources/js/Pages/Components/Timeline/Chapter.jsx
@@ -5,9 +5,13 @@ export default function Chapter({
     date = "September 2021",
     description = "Excepteur anim elit magna ea. Ullamco ullamco culpa non tempor dolore culpa consequat ut veniam. Proident est eiusmod esse labore laborum ex mollit ea commodo in occaecat eiusmod.",
     idx = 1,
+    first = false,
+    last = false,
 }) {
     return (
-        <div className="mx-auto max-w-screen-2xl max-lg:hidden">
+        <div
+            className={`mx-auto max-w-screen-2xl ${first ? "" : "-translate-y-5"} max-lg:hidden`}
+        >
             <div
                 className={`flex ${calculateLeft(idx) ? "" : "flex-row-reverse"} items-center justify-center gap-8`}
             >
@@ -21,7 +25,10 @@ export default function Chapter({
                     </div>
                 </div>
 
-                <div className="h-5 w-5 rounded-full bg-gradient-to-b from-gradient-from to-gradient-to shadow-chapter shadow-gradient-from/10"></div>
+                <div className="grid place-items-center">
+                    <div className="z-10 h-5 w-5 rounded-full bg-gradient-to-b from-gradient-from to-gradient-to shadow-chapter shadow-gradient-from/10"></div>
+                    <div className="absolute z-0 h-12 w-1 translate-y-1/2 bg-bg-green-light"></div>
+                </div>
 
                 <div
                     className={`${calculateLeft(idx) ? "" : "text-right"} flex-1`}
@@ -38,12 +45,12 @@ export default function Chapter({
                 className={`flex ${calculateLeft(idx) ? "" : "flex-row-reverse"} justify-center gap-8`}
             >
                 <p
-                    className={`${calculateLeft(idx) ? "text-right" : "text-left"} flex-1 pb-16 pt-4 text-project-description font-light text-text-green `}
+                    className={`${calculateLeft(idx) ? "text-right" : "text-left"} flex-1 ${last ? "pb-4" : "pb-16"} pt-4 text-project-description font-light text-text-primary `}
                 >
                     {description}
                 </p>
                 <div className="flex w-5 justify-center">
-                    <div className=" border border-bg-green-light"></div>
+                    <div className=" w-1 rounded-full bg-bg-green-light"></div>
                 </div>
                 <div className="flex-1"></div>
             </div>
