9b030ad716084e63b6332ab7d5c5039b7d4b17d1
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-06-11 16:54:55 +0300

    Timeline finished

--- resources/js/Pages/Components/Timeline.jsx
+++ resources/js/Pages/Components/Timeline.jsx
@@ -4,14 +4,36 @@ import Chapter from "./Timeline/Chapter";
 import ChapterMobile from "./Timeline/ChapterMobile";
 
 export default function Timeline() {
+    const timelineData = [
+        {
+            date: "May 2016",
+            title: "Start of the journey",
+            description:
+                "This moment marked the beginning of my journey into programming, setting the stage for my passion and career in technology.",
+
+            first: true,
+        },
+        {
+            date: "Semptember 2021",
+            title: "Web dev",
+            description:
+                "After enrolling in technical school, I developed a keen interest in creating web applications.",
+            last: true,
+        },
+    ];
+
     return (
         <>
             <Wave1 className="mt-4 translate-y-1 fill-bg-green" />
-            <div className="bg-bg-green pt-1 text-white">
-                <Chapter date="May 2016" idx={1} first={true} />
-                <ChapterMobile date="May 2016" first={true} />
-                <Chapter date="September 2021" last={true} idx={2} />
-                <ChapterMobile date="September 2021" last={true} />
+            <div className="bg-bg-green pt-8 text-white">
+                {timelineData.map((chapter, idx) => {
+                    return (
+                        <>
+                            <Chapter idx={idx + 1} {...chapter} />
+                            <ChapterMobile {...chapter} />
+                        </>
+                    );
+                })}
             </div>
         </>
     );
