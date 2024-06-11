80bf2d50f0704a5fbeea9324cbd6679207889f98
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-06-11 13:45:27 +0300

    Small test

--- resources/js/Pages/Components/Timeline.jsx
+++ resources/js/Pages/Components/Timeline.jsx
@@ -7,7 +7,7 @@ export default function Timeline() {
         <>
             <Wave1 className="mt-4 translate-y-1 fill-bg-green" />
             <div className="bg-bg-green pt-1 text-white">
-                <Chapter date="May 2016" />
+                <Chapter date="May 2016" idx={1} />
                 <Chapter date="September 2021" idx={2} />
             </div>
         </>
