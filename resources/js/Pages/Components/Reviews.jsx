e2b81757d232a4a74e2528aa5b55ca7895a04773
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-06-14 18:46:19 +0300

    Made windowDimension hook

--- resources/js/Pages/Components/Reviews.jsx
+++ resources/js/Pages/Components/Reviews.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import IconReviews from "../Icons/IconReviews";
 import TypeComponent from "./TypeComponent";
 import ReviewCard from "./ReviewCard";
+import useWindowDimensions from "./Hook/useWindowDimensions";
 
 export default function Reviews({ reviews = [] }) {
+    const { width } = useWindowDimensions();
+    // useEffect(() => console.log(width), [width]);
+
     return (
         <div className="container max-w-screen-2xl px-2">
             <div className="flex gap-4">
@@ -14,10 +18,12 @@ export default function Reviews({ reviews = [] }) {
                     className="mt-3 text-3xl font-bold text-bg-green-light/95 sm:mt-[10px] sm:text-title"
                 />
             </div>
-            <div className="mt-16 flex gap-8">
-                {reviews.map((review, idx) => (
-                    <ReviewCard {...review} key={`review-${idx}`} />
-                ))}
+            <div className=" mt-16 flex gap-8">
+                <div className="review-container">
+                    {reviews.map((review, idx) => (
+                        <ReviewCard {...review} key={`review-${idx}`} />
+                    ))}
+                </div>
             </div>
         </div>
     );
