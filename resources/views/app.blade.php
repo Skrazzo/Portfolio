228b71a8beb4b918f934652c5a1bb81765463b0b
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-06-23 13:09:47 +0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Leons Aleksandrovs</title>
+        <meta name="description" content="Full stack developer portfolio">
+        <meta name="keywords" content="Full stack developer, React, Laravel">
+        <meta name="author" content="Leons Aleksandrovs">
         @viteReactRefresh 
         @vite(['resources/scss/app.scss', 'resources/js/app.jsx'])
         <!-- As you can see, we will use vite with jsx syntax for React-->
