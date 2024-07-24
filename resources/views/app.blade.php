b000d0537f56802e3125cca320101dd3e9bbdd95
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-07-24 10:17:46 +0300

    Updated meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Leons Aleksandrovs</title>
-        <meta name="description" content="Full stack developer portfolio">
-        <meta name="keywords" content="Full stack developer, React, Laravel">
-        <meta name="author" content="Leons Aleksandrovs">
-        @viteReactRefresh 
+        <meta property="og:title" content="Leon - Full-stack developer">
+        <meta property="og:description" content="Full stack developer with over <?= date('Y') - 2020; ?> years of experience, and big amount of web skills">
+        <meta property="og:keywords" content="Full stack developer, React, Laravel, HTML, CSS, JavaScript, PHP">
+        <meta property="og:author" content="Leons Aleksandrovs">
+
+        @viteReactRefresh
         @vite(['resources/scss/app.scss', 'resources/js/app.jsx'])
         <!-- As you can see, we will use vite with jsx syntax for React-->
         @routes
